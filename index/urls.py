265c1563d25d285d9af7b2fa4a85b1c3b17ad45d
Lucas Simpson <Lucas Simpson>
2016-12-08 18:00:03 -0500

    some actual work lol

--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import IndexView
+from .views import IndexView, WorkExperienceView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^work_experience/$', WorkExperienceView.as_view(), name='work_experience'),
 ]
--- index/views.py
+++ index/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 # landing page
 class IndexView(TemplateView):
     template_name = 'index/index.html'
+
+
+# work experience
+class WorkExperienceView(TemplateView):
+    template_name = 'work_experience/work_experience.html'
