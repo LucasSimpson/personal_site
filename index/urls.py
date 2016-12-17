b9a5b056d8c5786c9964f1c5fa7b303d59e8d2c1
Lucas Simpson <lucassimpson05@gmail.com>
2016-12-17 12:44:54 -0500

    added angular frontend to be served, removed unneeded staticfiles

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 
-from .views import IndexView, WorkExperienceView
+from .views import IndexView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^work_experience/$', WorkExperienceView.as_view(), name='work_experience'),
 ]
--- index/views.py
+++ index/views.py
@@ -4,8 +4,3 @@ from django.views.generic import TemplateView
 # landing page
 class IndexView(TemplateView):
     template_name = 'index/index.html'
-
-
-# work experience
-class WorkExperienceView(TemplateView):
-    template_name = 'work_experience/work_experience.html'
