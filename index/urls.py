8710cd9801e63d21c8d30d07bb5dc729316fd1b5
Lucas Simpsone <lucassimpson05@gmail.com>
2017-04-08 18:13:07 -0400

    updated with KLU demo page for fun

--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import IndexView
+from .views import IndexView, KluView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^klu/(?P<message>.*)$', KluView.as_view(), name='klu'),
 ]
--- index/views.py
+++ index/views.py
@@ -1,6 +1,23 @@
 from django.views.generic import TemplateView
 
+from klu_pythonapi.notifications import push
+
 
 # landing page
 class IndexView(TemplateView):
     template_name = 'index/index.html'
+
+
+class KluView(TemplateView):
+    """KLU Demo view. Just says some message, and pushes a KLU notification."""
+
+    template_name = 'index/klu.html'
+
+    def get_context_data(self, **kwargs):
+        msg = kwargs.get('message', None)
+        if msg:
+            push('lucassimpson.com', 'Demo Success', msg)
+
+        return {
+            'message': kwargs.get('message', 'There is no message :(')
+        }
