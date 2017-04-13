eb57dad34217121a10edc50dae5a5a6c1f4d2934
Lucas Simpsone <lucassimpson05@gmail.com>
2017-04-13 14:07:23 -0400

    removed glyphs, over design. switched to server-side rendering

--- alt/views.py
+++ alt/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-
 from django.views.generic import TemplateView
+
+from funlinks import FunLink
+from workexperience import WorkExperience
 
 
 class IndexView(TemplateView):
     template_name = 'alt/index/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['fun_links'] = FunLink.all()
+        return context
+
 
 class WorkExperienceView(TemplateView):
-    template_name = 'alt/index/index.html'
+    template_name = 'alt/work_experience/work_experience.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(WorkExperienceView, self).get_context_data(**kwargs)
+        context['work_experiences'] = WorkExperience.all()
+        return context
